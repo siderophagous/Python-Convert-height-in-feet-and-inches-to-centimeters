a60a3e11fb5c3ac0d6a47ff748419776b9445b9b
shrimpkinn <92272085+shrimpkinn@users.noreply.github.com>
2021-10-11 20:16:07 +0530

    Create thescript.py

---
+++ thescript.py
@@ -0,0 +1,8 @@
+<?php   
+// This is a PHP program which converts the centimeter length to feet and Inches   
+$cen = 10;   
+$inch = 0.3937 * $cen;   
+$feet = 0.0328 * $cen;   
+echo("Inches is: " . $inch . "\n");   
+echo("Feet is: " . $feet);    
+?>   
